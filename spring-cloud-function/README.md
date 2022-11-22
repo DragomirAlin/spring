8d3da0419ffcdf865c68914848d79d46c0569558
Dragomir Alin <dragomirdanielalin@gmail.com>
2022-11-22 20:04:23 +0200

    add knative config and readme with commands

---
+++ spring-cloud-function/README.md
@@ -0,0 +1,10 @@
+# Spring Cloud Function
+Cloud Native Dev Hands-on 
+## Buildpacks ([https://buildpacks.io/docs/tools/pack/](https://buildpacks.io/docs/tools/pack/))
+```shell
+brew install buildpacks/tap/pack
+pack build parser-service --builder paketobuildpacks/builder:base --env BP_JVM_VERSION=17
+```
+## GraalVM Native Image
+
+## Knative ([https://knative.dev/](https://knative.dev/))
--- spring-cloud-function/src/main/java/ro/dragomiralin/serverless/ServerlessApplication.java
+++ spring-cloud-function/src/main/java/ro/dragomiralin/serverless/ServerlessApplication.java
@@ -2,12 +2,32 @@ package ro.dragomiralin.serverless;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
 
 @SpringBootApplication
 public class ServerlessApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerlessApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerlessApplication.class, args);
+    }
+
+    @Bean
+    public Function<String, String> hello() {
+        return (in) -> {
+            return "Hello: " + in + ", Source: ";
+        };
+    }
+
+    @Bean
+    public Function<String, String> uppercase() {
+        return value -> value.toUpperCase();
+    }
+
+    @Bean
+    public Function<String, String> addsample() {
+        return value -> "Hello " + value;
+    }
 
 }
