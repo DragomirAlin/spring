07a784287ed0d2ddfa55c9a43f1df1344e7b8b76
Dragomir Alin <dragomirdanielalin@gmail.com>
2023-05-21 14:59:06 +0300

    add retry

---
+++ retry/src/test/java/ro/dragomiralin/retry/RetryApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.dragomiralin.retry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
