838f25025af4d44dd9f551ae9dbe00066a099ffd
Dragomir Alin <dragomirdanielalin@gmail.com>
2023-05-21 13:42:40 +0300

    add bulkhead setup

---
+++ bullkhead/src/main/java/ro/dragomiralin/bullkhead/BullkheadApplication.java
@@ -0,0 +1,13 @@
+package ro.dragomiralin.bullkhead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullkheadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullkheadApplication.class, args);
+	}
+
+}
---
+++ bullkhead/src/test/java/ro/dragomiralin/bullkhead/BullkheadApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.dragomiralin.bullkhead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullkheadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
